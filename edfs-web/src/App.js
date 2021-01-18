60e106af5b720c2ce347dc91e9dfe0ee7774a1cf
sarahtartaruga <fannyFlye@gmx.de>
2021-01-18 16:32:49 +0100

    Added markdown to React

--- edfs-web/src/App.js
+++ edfs-web/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import AppMarkdown from "./EDSF.wiki/Home.md";
+import ReactMarkdown from "react-markdown";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { markdown: "" };
+  }
+
+  componentWillMount() {
+    // Get the contents from the Markdown file and put them in the React state, so we can reference it in render() below.
+    fetch(AppMarkdown)
+      .then((res) => res.text())
+      .then((text) => this.setState({ markdown: text }));
+  }
+
+  render() {
+    const { markdown } = this.state;
+    return <ReactMarkdown source={markdown} />;
+  }
 }
 
 export default App;
